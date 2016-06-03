3300e5ea557b0bfd4bf7d204077dd32e6929f6c9
Simon Engledew <simon@engledew.com>
2016-06-03 14:06:21 +0100

    Use css modules

--- config.js
+++ config.js
@@ -69,7 +69,10 @@ module.exports = function (options) {
 
   loaders.push({
     test: /\.css$/,
-    loader: ExtractTextPlugin.extract('style-loader', 'css-loader', 'postcss-loader')
+    loader: ExtractTextPlugin.extract(
+      'style-loader',
+      'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss-loader'
+    )
   })
 
   loaders.push({
