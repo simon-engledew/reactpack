ba6f30c3673ee36f292311cd519d2fb1fda9e372
Darren Royle <darren@easyreadsystem.com>
2016-06-15 09:08:05 +0100

    Added browser history change to deal with react-router reloads

--- dev.js
+++ dev.js
@@ -10,11 +10,11 @@ module.exports = function (config, options) {
   })
 
   var compiler = webpack(config)
-
+  var historyApiFallback = options.subpath ? { index: options.subpath } : true
   var server = new WebpackDevServer(compiler, {
     contentBase: config.output.path,
-    historyApiFallback: true,
     publicPath: options.subpath,
+    historyApiFallback: historyApiFallback,
     watchOptions: {
       aggregateTimeout: 300
     },
